c4740dfbbc345f951a46191bcf725d9dfb708a80
whh32 <am.lashkari2017@gmail.com>
2022-08-25 15:17:29 +0430

    Create routes

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home';
 
 const Routers = () => {
     return (
         <div className='pt-20 lg:pt-0'>
-            <div>
-                routers
-            </div>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='*' element={<h1>چیزی اینجا نیست!</h1>} />
+            </Routes>
         </div>
     );
 };
