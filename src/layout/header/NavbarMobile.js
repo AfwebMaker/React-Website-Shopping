0ea4e3c4f4a2e89ef304d8567f5f4942829f6d87
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-08-28 18:35:26 +0430

    updating navbar mobile

--- src/layout/header/NavbarMobile.js
+++ src/layout/header/NavbarMobile.js
@@ -1,35 +1,61 @@
 import React, { useState } from "react";
-// react-router-dom
-// import {Link} from 'react-router-dom'
 
+// react-router-dom
+import { Link } from "react-router-dom";
 // headlessui
 import { Disclosure, Transition } from "@headlessui/react";
 //react-icons
 import { IoIosArrowDown } from "react-icons/io";
 import { FiSearch } from "react-icons/fi";
 
-const NavbarMobile = ({ menu, navbarContent }) => {
+const profileItem = [
+  {
+    id: 1,
+    title: "پروفایل",
+  },
+  {
+    id: 2,
+    title: "ادیت پروفایل",
+  },
+  {
+    id: 3,
+    title: "علاقه مندی ها",
+  },
+  {
+    id: 4,
+    title: "مجموعه ها",
+  },
+  {
+    id: 5,
+    title: "تنظیمات اکانت",
+  },
+  {
+    id: 6,
+    title: "خروج",
+  },
+];
 
+const NavbarMobile = ({ menu, navbarContent }) => {
   const [value, setValue] = useState("");
   const changeHandler = (e) => {
-    setValue(e.target.value)
+    setValue(e.target.value);
   };
 
   return (
     <div className={menu ? "slidingNavbar" : "slidingNavbar -right-[1000px]"}>
-        <div className=" form-group relative px-2 w-full">
-                <input
-                  type="text"
-                  value={value}
-                  onChange={changeHandler}
-                  className="flex w-full h-10 pr-10 pl-4 py-3 text-sm font-normal placeholder:text-gray-400 rounded-lg bg-color-bg-search-navbar bg-clip-padding transition-all duration-300 caret-color-main-1 outline-none emailHelp123ase-in-out m-0 focus:ring-4 focus:ring-color-light-main-1 border focus:text-gray-600 focus:bg-color-main-3 focus:border-color-main-1"
-                  aria-describedby="emailHelp123"
-                  placeholder="جستجو"
-                />
-                <FiSearch className="absolute top-[10px] right-5 w-5 h-5 text-gray-400" />
-        </div>
+      <div className=" form-group relative w-full max-w-3xl">
+        <input
+          type="text"
+          value={value}
+          onChange={changeHandler}
+          className="flex w-full h-10 pr-10 pl-4 py-3 text-sm font-normal placeholder:text-gray-400 rounded-lg bg-color-bg-search-navbar bg-clip-padding transition-all duration-300 caret-color-main-1 outline-none emailHelp123ase-in-out m-0 focus:ring-4 focus:ring-color-light-main-1 border focus:text-gray-600 focus:bg-color-main-3 focus:border-color-main-1"
+          aria-describedby="emailHelp123"
+          placeholder="جستجو"
+        />
+        <FiSearch className="absolute top-[10px] right-4 w-5 h-5 text-gray-400" />
+      </div>
       <ul className="flex w-full flex-col items-center">
-        <div className="w-full px-0 pt-16">
+        <div className="w-full px-0 pt-10">
           <div className="mx-auto w-full max-w-3xl rounded-lg bg-color-main-3 ">
             {navbarContent.map((item) => (
               <Disclosure key={item.id} as="div" className="mb-5 rounded-lg">
@@ -63,22 +89,40 @@ const NavbarMobile = ({ menu, navbarContent }) => {
             ))}
           </div>
         </div>
-        {/* {navbarContent.map((item) => (
-          <li
-            key={item.id}
-            className="text-sm font-medium text-color-Text2 flex items-center ml-1 justify-center rounded-lg overflow-hidden"
-          >
-            <Link
-              to=""
-              className="flex items-center justify-center px-4 lg:py-3 text-color-Text1 font-normal w-fit h-fit hover:bg-black hover:bg-opacity-5 transition-all duration-300"
-            >
-              {item.title}
-            </Link>
-          </li>
-        ))} */}
       </ul>
+      <hr className="w-full my-5" />
+      <div className="mt-10 w-full">
+        <div className="text-center cursor-pointer">
+          <img
+            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+            className="rounded-full w-32 mb-4 mx-auto"
+            alt="Avatar"
+          />
+          <h5 className="text-xl font-medium leading-tight mb-2">John Doe</h5>
+          <p className="text-gray-500">Web designer</p>
+        </div>
+        <div className="grid grid-cols-12 my-10 border border-color-main-1 hover:ring-4 hover:ring-color-light-main-1 rounded-lg p-5 max-w-3xl m-auto transition-all duration-300">
+          {profileItem.map((item) => (
+            <span className="w-full col-span-6 px-16">
+              <Link
+                key={item.id}
+                to="/"
+                className={
+                  item.title === "خروج"
+                    ? " rounded-lg flex items-center justify-center w-full text-red-500  hover:bg-red-100 py-2 text-sm transition-all duration-200"
+                    : " rounded-lg flex items-center justify-center w-full text-color-Text2 hover:text-color-Text3 hover:bg-color-dark-main-2  py-2 text-sm transition-all duration-200"
+                }
+              >
+                {item.title}
+              </Link>
+            </span>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
+
+// className=" rounded-lg flex items-center justify-center w-full text-color-Text2 hover:text-color-Text3 hover:bg-color-dark-main-2  py-2 text-sm"
 
 export default NavbarMobile;
