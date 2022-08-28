8cdbc124496f7711dda113bcf760c4593c92583a
whh32 <am.lashkari2017@gmail.com>
2022-08-28 17:50:34 +0430

    chenge hamburger color

--- src/layout/header/Hamburger.js
+++ src/layout/header/Hamburger.js
@@ -4,9 +4,9 @@ const Hamburger = ({isOpen}) => {
 
     return (
         <div className='relative flex flex-col justify-center items-center h-[15px] w-[20px] cursor-pointer z-10'>
-            <span className={`transition-all duration-[100ms] absolute rounded-sm flex w-full h-[2px] bg-black ${isOpen ? 'rotate-[-45deg] ' : 'top-0'}`}></span>
-            <span className={`flex rounded-sm w-full h-[2px] bg-black ${isOpen ? 'hidden' : ''}`}></span>
-            <span className={`transition-all duration-[100ms] absolute rounded-sm flex w-full h-[2px] bg-black ${isOpen ? 'rotate-45 ' : 'bottom-0'}`}></span>
+            <span className={`transition-all duration-[100ms] absolute rounded-sm flex w-full h-[2px] bg-color-Text2 ${isOpen ? 'rotate-[-45deg] ' : 'top-0'}`}></span>
+            <span className={`flex rounded-sm w-full h-[2px] bg-color-Text2 ${isOpen ? 'hidden' : ''}`}></span>
+            <span className={`transition-all duration-[100ms] absolute rounded-sm flex w-full h-[2px] bg-color-Text2 ${isOpen ? 'rotate-45 ' : 'bottom-0'}`}></span>
         </div>
     )
 }
