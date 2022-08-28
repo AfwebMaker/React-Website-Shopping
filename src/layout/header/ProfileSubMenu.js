a93b6667c23d3bfa49233f5d94f636f5969fb4b9
whh32 <am.lashkari2017@gmail.com>
2022-08-28 17:40:22 +0430

    clear errors

--- src/layout/header/ProfileSubMenu.js
+++ src/layout/header/ProfileSubMenu.js
@@ -39,9 +39,9 @@ const ProfileSubMenu = () => {
                 <span className='w-full'>
                     حالت شب
                 </span>
-                <div class="flex justify-center">
-                    <div class="form-check form-switch">
-                        <input class="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+                <div className="flex justify-center">
+                    <div className="form-check form-switch">
+                        <input className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-gray-300 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
                     </div>
                 </div>
             </Link>
