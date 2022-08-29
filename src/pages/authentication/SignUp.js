9333aacb0d048a5440f0f453ca4c0cc0e5217e80
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-08-29 23:10:48 +0430

    updating

--- src/pages/authentication/SignUp.js
+++ src/pages/authentication/SignUp.js
@@ -89,7 +89,7 @@ const SignUp = () => {
                 </div>
 
                 <a
-                  className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3 bg-[#3b5998]"
+                  className="px-7 py-3 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3 bg-[#3b599]"
                   // style="background-color: #3b5998"
                   href="#!"
                   role="button"
